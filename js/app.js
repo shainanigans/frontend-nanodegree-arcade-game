130dc99e2e2614a7116cb95aa4312db46301b15e
shainanigans <me@shainakoval.com>
2015-11-19 10:21:45 +1100

    Attempt collision detection with x and y vals

--- js/app.js
+++ js/app.js
@@ -1,27 +1,35 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, distance, start) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
-    this.y = y;
-    this.distance = distance; //determines how far enemy moves per second
-    this.start = start; //delays start time so they stagger entrance
+
+    //Set random row to start on
+    var startingRows = [215, 135, 55]; //valid y values of enemy rows
+    var randomRow = Math.floor(Math.random() * startingRows.length);
+    this.y = startingRows[randomRow];
+
+    //Determine how many pixels enemy moves per second
+    this.distance = Math.floor(Math.random() * 10 + 1) * 100; //pixels
+    //Cap max speed
+    if (distance > 500) {
+        distance = 500;
+    };
+
+    //Delay start time so enemies stagger entrance
+    this.start = Math.floor(Math.random() * 10000); //ms
+
+    //Set image
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    //var move = setTimeout(function() {
-        if (this.x < 502) {
-            this.x = this.x + this.distance * dt;
-        } else {
-            this.x = -100; //moves enemy back to start for looping
-        }
-    //}, 1000);
+    if (this.x < 502) {
+        //TODO: get the delay to work
+        setTimeout(this.x = this.x + this.distance * dt, this.start);
+    } else {
+        this.x = -100; //moves enemy back to start for looping
+    }
 };
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -62,22 +70,7 @@ var allEnemies = [];
 
 var makeEnemies = function() {
     for (i = 0; i <= 10; i++) {
-        var startingRows = [215, 135, 55]; //valid y values of enemy rows
-        var randomRow = Math.floor(Math.random() * startingRows.length);
-
-        //Generate random speeds and starting rows
-        var distance = Math.floor(Math.random() * 10 + 1) * 100; //x values
-        //Cap max speed
-        if (distance > 500) {
-            distance = 500;
-        };
-
-        //Determine start time
-        //TODO: Get this to work
-        var start = Math.floor(Math.random() * 10);
-
-        var enemy = new Enemy(-100, startingRows[randomRow], distance, start);
-
+        var enemy = new Enemy(-100, this.y, this.distance, this.start);
         allEnemies.push(enemy);
     }
 };
@@ -87,13 +80,38 @@ var player = new Player(202, 375);
 
 //Tracks the x and y values of enemies for collision detection
 //Enemy.prototype.trackCollision = function() {
-//    if (player.x === this.x && player.y === this.y) {
-//        console.log('collision!');
-//    }
+//    //setInterval(function() {
+//        if (player.x === this.x && player.y === this.y) {
+//            console.log('collision!');
+//        }
+//    //}, 100);
 //};
 //allEnemies.forEach(function(enemy) {
-//    enemy.trackCollision();
+//    setInterval(enemy.trackCollision(), 100);
 //});
+var trackCollisions = function() {
+    var enemyX = [];
+    var enemyY = [];
+    //setInterval(function() {
+        for (i = 0; i < allEnemies.length; i++) {
+            enemyX.push(allEnemies[i].x);
+            enemyY.push(allEnemies[i].y);
+        };
+    //}, 100);
+    console.log(enemyX);
+    if (enemyX.length > 10) {
+        console.log('too long');
+        enemyX.splice(-1, 1);
+        enemyY.splice(-1, 1);
+    }
+            console.log(enemyX);
+    for (i = 0; i < allEnemies.length; i++) {
+        if (player.x === enemyX[i] && player.y === enemyY[i]) {
+            console.log('collision!');
+        }
+    }
+};
+trackCollisions();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
